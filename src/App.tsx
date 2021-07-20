46fd398a1f3a9438b8217a07c9f8132303551a9f
Paweł Stech <pstech@speednet.pl>
2021-07-20 14:31:05 +0200

    fnc and DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // function addition(firstNumber:number | string =0, secondNumber:number |string =0) {
+    //  const a = typeof firstNumber === 'number' ? firstNumber : Number(firstNumber)
+    //     const b = typeof secondNumber === 'number' ? secondNumber : Number(firstNumber)
+    //
+    //     return a+ b;
+    //
+    // }
+    function addition(firstNumber:number | string =0, secondNumber:number |string =0) {
+        const a = typeof firstNumber === 'number' ? firstNumber : Number(firstNumber)
+        const b = typeof secondNumber === 'number' ? secondNumber : Number(firstNumber)
+
+        return a+ b;
+        
+    }
+    const result = addition(2,'3')
+    console.log(result)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Ts</p>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
